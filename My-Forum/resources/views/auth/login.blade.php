9f8ea5b1717245cf6366b968d3e0f9c03b3199d1
Leven <leven.z@xuea100.com>
2016-12-09 15:05:10 +0800

    修改登录页面

--- My-Forum/resources/views/auth/login.blade.php
+++ My-Forum/resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    {{--<div class="main layui-clear">
+    <div class="main layui-clear">
         <h2 class="page-title">登入</h2>
         <div class="layui-form layui-form-pane">
             <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
@@ -10,40 +10,41 @@
                 <div class="layui-form-item has-error">
                     <label for="L_email" class="layui-form-label">邮箱</label>
                     <div class="layui-input-inline">
-                        <input type="text" id="L_email" name="email" required="" lay-verify="required" autocomplete="off" class="layui-input">
+                        <input type="email" id="L_email" name="email" required="" lay-verify="required" value="{{ old('email') }}" required autofocus autocomplete="off" class="layui-input">
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                        @endif
                     </div>
                 </div>
                 <div class="layui-form-item">
                     <label for="L_pass" class="layui-form-label">密码</label>
                     <div class="layui-input-inline">
-                        <input type="password" id="L_pass" name="pass" required="" lay-verify="required" autocomplete="off" class="layui-input">
+                        <input type="password" id="L_pass" name="password" required="" lay-verify="required" autocomplete="off" class="layui-input" required>
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                        @endif
                     </div>
                 </div>
-                --}}{{--<div class="layui-form-item">
-                    <label for="L_vercode" class="layui-form-label">人类验证</label>
-                    <div class="layui-input-inline">
-                        <input type="text" id="L_vercode" name="vercode" required="" lay-verify="required" placeholder="请回答后面的问题" autocomplete="off" class="layui-input">
-                    </div>
-                    <div class="layui-form-mid">
-                        <span style="color: #c00;"></span>
-                    </div>
-                </div>--}}{{--
                 <div class="layui-form-item">
                     <button type="submit" class="layui-btn" lay-filter="*" lay-submit="">立即登录</button>
         <span style="padding-left:20px;">
-          <a href="/user/forget">忘记密码？</a>
+          <a href="{{ url('/password/reset') }}">忘记密码？</a>
         </span>
                 </div>
-                --}}{{--<div class="layui-form-item fly-form-app">
+                {{--<div class="layui-form-item fly-form-app">
                     <span>或者使用社交账号登入</span>
                     <a href="" onclick="layer.msg('正在通过QQ登入', {icon:16, shade: 0.1, time:0})" class="iconfont icon-qq" title="QQ登入"></a>
                     <a href="" onclick="layer.msg('正在通过微博登入', {icon:16, shade: 0.1, time:0})" class="iconfont icon-weibo" title="微博登入"></a>
-                </div>--}}{{--
+                </div>--}}
             </form>
         </div>
-    </div>--}}
+    </div>
 
-<div class="container" style="padding-top: 70px;">
+{{--<div class="container" style="padding-top: 70px;">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -106,7 +107,7 @@
             </div>
         </div>
     </div>
-</div>
+</div>--}}
 
     @include('layouts.foot')
 
