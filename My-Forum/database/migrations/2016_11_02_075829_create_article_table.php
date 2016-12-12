e8a0eed28292128a1df923fabfb48d9637a8fb48
Leven <leven.z@xuea100.com>
2016-12-12 09:15:36 +0800

    修改文章表字段

--- My-Forum/database/migrations/2016_11_02_075829_create_article_table.php
+++ My-Forum/database/migrations/2016_11_02_075829_create_article_table.php
@@ -13,15 +13,18 @@ class CreateArticleTable extends Migration
     {
         //
         Schema::create('articles', function (Blueprint $table) {
-            $table->increments('aid');
-            $table->integer('userid');
-            $table->string('title',50);
-            $table->text('content');
-            $table->integer('status')->default(0);#
-            $table->integer('isdel')->default(0);
-            $table->integer('tagid')->default(0);
-            $table->integer('type')->default(0);
-            $table->integer('clicknum')->default(0);
+            $table->increments('aid')->comment('ID');
+            $table->integer('userid')->comment('发布者用户ID');
+            $table->string('title',50)->comment('标题');
+            $table->text('content')->comment('内容');
+            $table->integer('reward')->comment('悬赏数');
+            $table->integer('status')->default(0)->comment('状态 0正常 1已采纳（完结） ');
+            $table->integer('isgood')->default(0)->comment('是否加精状态 0正常 1加精 ');
+            $table->integer('topNum')->default(0)->comment('置顶序号，值越大越靠前 ');
+            $table->integer('isdel')->default(0)->comment('删除状态 0正常 1删除');
+            $table->integer('tagid')->default(0)->comment('标签id');
+            $table->integer('type')->default(0)->comment('类型 0默认');
+            $table->integer('clicknum')->default(0)->comment('点击次数');
             $table->timestamps();
         });
     }
