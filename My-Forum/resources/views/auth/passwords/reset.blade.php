b2975291483abe420da6ac0e796c38488d723c74
leven_zeng <18707022909@163.com>
2016-12-26 22:56:22 +0800

    修改重置密码

--- My-Forum/resources/views/auth/passwords/reset.blade.php
+++ My-Forum/resources/views/auth/passwords/reset.blade.php
@@ -1,7 +1,71 @@
-@extends('layouts.app')
+@extends('layouts.head')
 
 @section('content')
-<div class="container">
+    <div class="main layui-clear">
+        <h2 class="page-title">重置密码</h2>
+        <div class="fly-msg">
+            请重置您的密码
+        </div>
+        <div class="layui-form layui-form-pane" style="margin-top: 30px;">
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
+                {{ csrf_field() }}
+
+                <input type="hidden" name="token" value="{{ $token }}">
+                {{--<div class="layui-form-item">--}}
+                    {{--<label for="L_pass" class="layui-form-label">邮箱</label>--}}
+                    {{--<div class="layui-input-inline">--}}
+
+                        {{--<input id="email" type="email"  class="layui-input" name="email"  value="{{ $email or old('email') }}" required autofocus>--}}
+
+                        {{--@if ($errors->has('email'))--}}
+                            {{--<span class="help-block">--}}
+                                        {{--<strong>{{ $errors->first('email') }}</strong>--}}
+                                    {{--</span>--}}
+                        {{--@endif--}}
+                    {{--</div>--}}
+                    {{--<div class="layui-form-mid layui-word-aux">--}}
+                        {{--6到16个字符--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+                <div class="layui-form-item">
+                    <label for="L_pass" class="layui-form-label">密码</label>
+                    <div class="layui-input-inline">
+                        {{--<input type="password" id="L_pass" name="pass" required="" lay-verify="required" autocomplete="off" class="layui-input" />--}}
+                        <input id="password" type="password"  name="password" class="layui-input" required>
+
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                        @endif
+                    </div>
+                    <div class="layui-form-mid layui-word-aux">
+                        6到16个字符
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <label for="L_repass" class="layui-form-label">确认密码</label>
+                    <div class="layui-input-inline">
+                        {{--<input type="password" id="L_repass" name="repass" required="" lay-verify="required" autocomplete="off" class="layui-input" />--}}
+                        <input id="password-confirm" type="password" name="password_confirmation" class="layui-input" required>
+
+                        @if ($errors->has('password_confirmation'))
+                            <span class="help-block">
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </span>
+                        @endif
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <input type="hidden" name="username" value="字恋狂" />
+                    <input type="hidden" name="email" value="1321101613@qq.com" />
+                    <button class="layui-btn" alert="1" lay-filter="*" lay-submit="">提交</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+{{--<div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -66,5 +130,7 @@
             </div>
         </div>
     </div>
-</div>
+</div>--}}
+
+    @include('layouts.foot')
 @endsection
