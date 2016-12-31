5ab281a7d7b902b4e132c169f0ece579414eb33c
leven_zeng <18707022909@163.com>
2016-12-31 19:01:41 +0800

    添加网站小logo

---
+++ My-Forum/resources/views/emails/test.blade.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Leven
+ * Date: 2016/12/31
+ * Time: 10:00
+ */?>
+
+<table border="0" cellpadding="0" cellspacing="0" style="width: 600px; border: 1px solid #ddd; border-radius: 3px; color: #555; font-family: 'Helvetica Neue Regular',Helvetica,Arial,Tahoma,'Microsoft YaHei','San Francisco','微软雅黑','Hiragino Sans GB',STHeitiSC-Light; font-size: 12px; height: auto; margin: auto; overflow: hidden; text-align: left; word-break: break-all; word-wrap: break-word;">
+    <tbody style="margin: 0; padding: 0;">
+    <tr style="background-color: #393D49; height: 60px; margin: 0; padding: 0;">
+        <td style="margin: 0; padding: 0;">
+            <div style="color: #5EB576; margin: 0; margin-left: 30px; padding: 0;">
+                <a style="font-size: 14px; margin: 0; padding: 0; color: #5EB576; text-decoration: none;" href="http://fly.layui.com/" target="_blank">追爱吧社区-重置密码</a>
+            </div> </td>
+    </tr>
+    <tr style="margin: 0; padding: 0;">
+        <td style="margin: 0; padding: 30px;"> <p style="line-height: 20px; margin: 0; margin-bottom: 10px; padding: 0;"> 请尽快重置您的密码： </p>
+            <div style="">
+                <a href="http://localhost:1024/password/reset/6fa0cf3f644d7316a5c8c746e4a2bc898f26c8d84282d2af8a66c0244a35c7b7" style="background-color: #009E94; color: #fff; display: inline-block; height: 32px; line-height: 32px; margin: 0 15px 0 0; padding: 0 15px; text-decoration: none;" target="_blank">立即重置密码</a>
+            </div> <p style="line-height: 20px; margin-top: 20px; padding: 10px; background-color: #f2f2f2; font-size: 12px;"> 如果该邮件不是由你本人操作，请忽视！ </p>
+            <p style="line-height: 20px; margin-top: 20px; padding: 10px; background-color: #f2f2f2; font-size: 12px;"> 点击重置密码按钮无法跳转时，您可以复制以下链接到您的浏览器中打开：<a href="http://localhost:1024/password/reset/6fa0cf3f644d7316a5c8c746e4a2bc898f26c8d84282d2af8a66c0244a35c7b7" target="_blank">http://local<wbr>host:1024/pa<wbr>ssword/reset<wbr>/6fa0cf3f644<wbr>d7316a5c8c74<wbr>6e4a2bc898f2<wbr>6c8d84282d2a<wbr>f8a66c0244a3<wbr>5c7b7</a>  </p>
+        </td>
+    </tr>
+    <tr style="background-color: #fafafa; color: #999; height: 35px; margin: 0; padding: 0; text-align: center;">
+        <td style="margin: 0; padding: 0;">系统邮件，请勿直接回复。{{$article->titles}}</td>
+        <td style="margin: 0; padding: 0;">{{date('Y-m-d H:i:s')}}</td>
+    </tr>
+    </tbody>
+</table>
